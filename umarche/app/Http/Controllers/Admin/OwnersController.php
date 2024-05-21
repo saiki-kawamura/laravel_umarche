62b7a75e6b872bf3a52fbf23adeafba6da30dae3
saiki-kawamura <kawamura@hiroginks.com>
2024-05-21 18:56:01 +0900

    “途中経過”

--- umarche/app/Http/Controllers/Admin/OwnersController.php
+++ umarche/app/Http/Controllers/Admin/OwnersController.php
@@ -24,13 +24,13 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $data_now = Carbon::now();
-        $data_parse = Carbon::parse(now());
-        echo $data_now->year;
-        echo $data_parse;
+        // $data_now = Carbon::now();
+        // $data_parse = Carbon::parse(now());
+        // echo $data_now->year;
+        // echo $data_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
         // $q_first =DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -38,7 +38,8 @@ class OwnersController extends Controller
         // ]);
 
         // dd($e_all,$q_get,$q_first,$c_test);
-        return view('admin.owners.index',compact('e_all','q_get'));
+        $owners = Owner::select('name','email','created_at')->get();
+        return view('admin.owners.index',compact('owners'));
     }
 
     /**
@@ -48,7 +49,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
